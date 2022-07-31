bea8a0a1e009e2d7b67a0337f76a19fa722258e6
Mohiuddin Shovon <shovon.du86@gmail.com>
2022-07-31 20:15:23 +0600

    Update __init__.py

--- app/mod_da/__init__.py
+++ app/mod_da/__init__.py
@@ -1,5 +1,1 @@
-# File Upload
-#Allowed file types only csv
-ALLOWED_EXTENSIONS = {'csv'}
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
