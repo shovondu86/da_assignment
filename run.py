648d2a278ac881bcff76be289589bba7d3356e95
Mohiuddin Shovon <shovon.du86@gmail.com>
2022-07-31 20:12:53 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,6 @@
+__author__ = "Mohiuddin Shovon"
+__date__ = "30 th July,2022"
+__email__ = "shovon.du86@gmail.com"
+# app run 8000 port. you can change it
 from app import app
 app.run(host='0.0.0.0', port=8000, debug=True)
